fada6032825a75523745156c7ae828b9d69688d1
dlzkap11 <40196658+dlzkap11@users.noreply.github.com>
2026-06-23 16:48:00 +0900

    [level 1] Title: [1차] 비밀지도, Time: 0.04 ms, Memory: 4.82 MB -BaekjoonHub

--- 프로그래머스/1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.cpp
+++ 프로그래머스/1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.cpp
@@ -5,39 +5,15 @@ using namespace std;
 
 vector<string> solution(int n, vector<int> arr1, vector<int> arr2) {
     vector<string> answer;
-    
-    // 십진수 -> 이진수로 변환
-    // 0 = "", 1 = "#"
-    // 9 -> 01001 (n = 5) 1001이지만 빈자리는 0으로 채우기
-    // 9 % 2 = 1 9 / 2 = 4
-    // 4 % 2 = 0 4 / 2 = 2
-    // 2 % 2 = 0 2 / 2 = 1
-    // 1 % 2 = 1 1 / 2 = 0
-    // 0 % 2 = 0 ---------
-    for(int i = 0; i < n; i++){
-        string map;
-        int aBin;
-        int bBin;
-        int a = arr1[i];
-        int b = arr2[i];
-        
-        for(int j = 0; j < n; j++){
-            aBin = (a % 2);
-            a = a / 2;
-            
-            bBin = (b % 2);
-            b = b / 2;
-            
-            if(aBin != bBin)
-                map = '#' + map;
-            else if(aBin == bBin && aBin == 0)
-                map = ' ' + map;
-            else //if(aBin == bBin && aBin == 1)
-                map = '#' + map;
+    for(int i=0; i <n; i++){
+        int bin = arr1[i]|arr2[i]; // 둘을 비교해서 둘 다 0이면 0반환 아니면 1반환
+        string ans = "";
+        for(int j = 0; j<n; j++){
+            if(bin % 2 == 0) ans = " " + ans;
+            else ans = "#" + ans;
+            bin = bin >> 1; //>> 1 == / 2
         }
-        answer.push_back(map);
+        answer.push_back(ans);
     }
-    
-    
     return answer;
 }
