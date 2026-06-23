20e6b3efa04a8af84ad0b35346e77401628bae50
dlzkap11 <40196658+dlzkap11@users.noreply.github.com>
2026-06-23 15:06:09 +0900

    [level 1] Title: 같은 숫자는 싫어, Time: 105.02 ms, Memory: 106 MB -BaekjoonHub

--- 프로그래머스/1/12906. 같은 숫자는 싫어/README.md
+++ 프로그래머스/1/12906. 같은 숫자는 싫어/README.md
@@ -1,10 +1,10 @@
 # [level 1] 같은 숫자는 싫어 - 12906 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12906) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12906#) 
 
 ### 성능 요약
 
-메모리: 106 MB, 시간: 102.22 ms
+메모리: 106 MB, 시간: 105.02 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2026년 06월 23일 15:03:34
+2026년 06월 23일 15:06:05
 
 ### 문제 설명
 
--- 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.cpp
+++ 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.cpp
@@ -1,21 +1,13 @@
 #include <vector>
 #include <iostream>
-
+#include <algorithm>
 using namespace std;
 
 vector<int> solution(vector<int> arr) 
 {
-    vector<int> answer;
 
-    for(int i = 0; i < arr.size() - 1; i++){
-        if(arr[i] == arr[i + 1]){
-            continue;
-        }
-        else{
-            answer.push_back(arr[i]);
-        }
-    }
-    answer.push_back(arr[arr.size()-1]);
+    arr.erase(unique(arr.begin(), arr.end()),arr.end());
 
+    vector<int> answer = arr;
     return answer;
 }
