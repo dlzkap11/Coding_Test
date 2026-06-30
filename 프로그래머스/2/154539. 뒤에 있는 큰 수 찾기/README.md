d0beae5cf1d9d7f249e9c9912073e62211153f33
dlzkap11 <40196658+dlzkap11@users.noreply.github.com>
2026-06-30 16:09:00 +0900

    [level 2] Title: 뒤에 있는 큰 수 찾기, Time: 15.90 ms, Memory: 129 MB -BaekjoonHub

--- 프로그래머스/2/154539. 뒤에 있는 큰 수 찾기/README.md
+++ 프로그래머스/2/154539. 뒤에 있는 큰 수 찾기/README.md
@@ -1,10 +1,10 @@
 # [level 2] 뒤에 있는 큰 수 찾기 - 154539 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/154539?language=csharp) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/154539?language=csharp#) 
 
 ### 성능 요약
 
-메모리: 129 MB, 시간: 16.15 ms
+메모리: 129 MB, 시간: 15.90 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2026년 06월 30일 16:05:21
+2026년 06월 30일 16:08:57
 
 ### 문제 설명
 
--- 프로그래머스/2/154539. 뒤에 있는 큰 수 찾기/뒤에 있는 큰 수 찾기.cs
+++ 프로그래머스/2/154539. 뒤에 있는 큰 수 찾기/뒤에 있는 큰 수 찾기.cs
@@ -3,19 +3,36 @@ using System.Collections.Generic;
 
 public class Solution {
     public int[] solution(int[] numbers) {
-        int n = numbers.Length;
-        int[] answer = new int[n];
-        Array.Fill(answer, -1);
+        int[] answer = new int[numbers.Length]; 
+        Stack<int> stack = new Stack<int>();
 
-        Stack<int> stack = new Stack<int>(); // 인덱스 저장
-
-        for (int i = 0; i < n; i++) {
-            while (stack.Count > 0 && numbers[stack.Peek()] < numbers[i]) {
-                answer[stack.Pop()] = numbers[i];
+        for(int i = numbers.Length-1 ; i>=0 ; i--)
+        {
+            if(stack.Count ==0)
+            {
+                answer[i]=-1;
+                stack.Push(numbers[i]);
+                continue;
             }
-            stack.Push(i);
-        }
 
+            while(stack.Count>0)
+            {
+                if(stack.Peek()>numbers[i])
+                {
+                    answer[i]=stack.Peek();
+                    stack.Push(numbers[i]);                    
+                    break;
+                }
+                stack.Pop();
+            }
+
+            if(answer[i]==0)
+            {
+                answer[i]=-1;
+                stack.Push(numbers[i]);
+            }
+
+        }
         return answer;
     }
 }
