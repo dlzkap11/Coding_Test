02aa2b03fdafee5835e04f07d7eb550b439d485f
dlzkap11 <40196658+dlzkap11@users.noreply.github.com>
2026-07-14 12:51:28 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 48.96 ms, Memory: 25.6 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.cpp
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.cpp
@@ -1,34 +1,18 @@
 #include <string>
 #include <vector>
-#include <unordered_map>
+#include <algorithm>
 
 using namespace std;
 
 string solution(vector<string> participant, vector<string> completion) {
     string answer = "";
-    
-    // 맵에다가 선수들을 추가
-    unordered_map<string,int> um;
-    for(int i = 0; i < participant.size(); i++){
-        if(um.find(participant[i]) != um.end())
-            um[participant[i]]++;
-        else
-            um.insert({participant[i], 1});
+    sort(participant.begin(), participant.end());
+    sort(completion.begin(), completion.end());
+    for(int i=0;i<completion.size();i++)
+    {
+        if(participant[i] != completion[i])
+            return participant[i];
     }
-    
-    // 완주한 선수들을 한명씩 제거(죽이는거 아님)
-    for(int i = 0; i < completion.size(); i++){
-        if(um.find(completion[i]) != um.end())
-            um[completion[i]]--;
-    }
-    
-    for(pair<string,int> elem : um){
-        if(elem.second == 1){
-            answer = elem.first;
-            break;
-        }
-            
-    }
-    
-    return answer;
+    return participant[participant.size() - 1];
+    //return answer;
 }
