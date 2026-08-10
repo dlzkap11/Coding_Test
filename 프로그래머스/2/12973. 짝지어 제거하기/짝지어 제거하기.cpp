fedd3647f0033f36d4ab7b77aece69f6d032bc56
dlzkap11 <40196658+dlzkap11@users.noreply.github.com>
2026-08-10 17:29:51 +0900

    [level 2] Title: 짝지어 제거하기, Time: 3.44 ms, Memory: 6.42 MB -BaekjoonHub

--- 프로그래머스/2/12973. 짝지어 제거하기/짝지어 제거하기.cpp
+++ 프로그래머스/2/12973. 짝지어 제거하기/짝지어 제거하기.cpp
@@ -1,22 +1,15 @@
-#include <iostream>
-#include<string>
-#include <stack>
+#include <string>
 using namespace std;
 
-int solution(string s)
-{
-    stack<char> st;
-    
-    for (char c : s) {
-        if (!st.empty() && st.top() == c) {
-            st.pop();  // 같은 문자면 제거
-        } else {
-            st.push(c);  // 다르면 스택에 추가
+int solution(string s) {
+    int i = 0;
+    for (const char ch : s) {
+        if (i > 0 && s[i - 1] == ch)
+            i--;  
+        else {
+            s[i] = ch;
+            i++;
         }
     }
-    
-    return st.empty() ? 1 : 0;
-    
-    
-
+    return !i;
 }
